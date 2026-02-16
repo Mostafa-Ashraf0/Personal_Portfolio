114b362114a504c04d798b4e6a68237ebd7ed046
Mostafa-Ashraf0 <mostafaashrof158@gmail.com>
2026-02-17 01:53:32 +0200

    media queries

--- app/components/header.js
+++ app/components/header.js
@@ -1,6 +1,11 @@
 'use client';
 import style from '../../styles/header.module.css';
 import { useRouter } from 'next/navigation';
+import home from '../../public/home-1393-svgrepo-com.svg';
+import projects from '../../public/bag-svgrepo-com.svg';
+import contact from '../../public/contact-mail-svgrepo-com.svg';
+import blog from '../../public/blog-svgrepo-com.svg';
+import Image from 'next/image';
 
 const Header = ()=>{
     const router = useRouter();
@@ -15,13 +20,45 @@ const Header = ()=>{
     }
     return(
         <div className={style.header}>
-            <span>M.A</span>
+            <span className={style.icon}>M.A</span>
             <nav>
                 <ul>
-                    <li onClick={goToHome} >Home</li>
-                    <li onClick={goToProjects}>Projects</li>
-                    <li onClick={goToContact}>Contact</li>
-                    <li>Blog</li>
+                    <li onClick={goToHome}>
+                        <Image
+                        src={home}
+                        alt=''
+                        width={0}
+                        height={0}
+                        />
+                        <span>Home</span>
+                    </li>
+                    <li onClick={goToProjects}>
+                        <Image
+                        src={projects}
+                        alt=''
+                        width={0}
+                        height={0}
+                        />
+                        <span>Projects</span>
+                    </li>
+                    <li onClick={goToContact}>
+                        <Image
+                        src={contact}
+                        alt=''
+                        width={0}
+                        height={0}
+                        />
+                        <span>Contact</span>
+                    </li>
+                    <li>
+                        <Image
+                        src={blog}
+                        alt=''
+                        width={0}
+                        height={0}
+                        />
+                        <span>Blog</span>
+                    </li>
                 </ul>
             </nav>
         </div>
