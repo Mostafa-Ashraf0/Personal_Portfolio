73c765090e76ecb7260492141777b3c806d90f1b
Mostafa-Ashraf0 <mostafaashrof158@gmail.com>
2026-02-19 23:45:11 +0200

    projectImage bug fix

--- app/features/getProjectImage.js
+++ app/features/getProjectImage.js
@@ -2,7 +2,7 @@ const url = process.env.NEXT_PUBLIC_API_URL;
 
 const getProjectImages = async(id)=>{
     try{
-        const res = await fetch(`http://localhost:1337/api/projects?filters[id][$eq]=${id}&populate[projects_images][populate]=image`);
+        const res = await fetch(`${url}/api/projects?filters[id][$eq]=${id}&populate[projects_images][populate]=image`);
         if (!res.ok) throw new Error('failed to load Projects');
         const data = await res.json();
         return data;
